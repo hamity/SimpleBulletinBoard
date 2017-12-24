fda3627bc262437f8a104d09532ae29263f7988d
Horie Kazuaki <hazemi94basic@gmail.com>
2017-12-24 18:33:16 +0900

    簡易の確率によって投稿の文字のサイズが変化する掲示板を製作
    完成した掲示板のCentOSフォルダ全体をgithubに上げたが、同じ仮想サーバー内で別のプロジェクト立ち上げたいと思ったため、簡易掲示板に該当するファイルのみ再アップロード

---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+//SQLに使用する用語の定義
+define("DB_HOST", "localhost");
+define("DB_NAME", "bbs_php");
+define("DB_CHARSET", "utf8");
+define("DB_USER", "root");
+define("DB_PASSWORD", "BBS_php17");
+define("DB_TABLE", "posts");
+
+try{
+    //DBへの接続
+    $db = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset='.DB_CHARSET, DB_USER, DB_PASSWORD,
+    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));        //DB接続できた物に対する、操作を行う.今回はエラーレポートに対し、例外を投げるように設定している(これにより try, catchが使用可能)
+
+    $db_post = $db->query("select * from posts");
+
+    $db = null;
+}
+catch(PODException $e){
+    echo "Error: read mysql";
+    exit('データベース接続失敗'.$e->getMessage());
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>簡易掲示板</title>
+</head>
+<body>
+    <h1>簡易掲示板</h1>
+    <form action="./input.php" method="post">
+        message: <input type="text" name="message" contents="no-cache">
+        user: <input type="text" name="user" contents="no-cache">
+        <input type="hidden" name="url" value=<?php print((empty($_SERVER["HTTPS"]) ? "http://" : "https://").$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]);?>>
+        <input type="submit" value="投稿">
+    </form>
+    <h2>投稿件数(
+        <?php
+        $postsNum = $db_post->rowCount();
+        echo (int)$postsNum;
+        ?>件)</h2>
+    <ul>
+        <?php
+        if($postsNum > 0){
+            foreach($db_post as $post){
+                echo ShowPostData($post, (int)$post["rarity"]);
+            }
+        }
+        else if($postsNum === 0){
+            echo "まだ投稿はありません";
+        }
+        else{
+            echo "Error: $postNumに不正な値が入力されています";
+        }
+        ?>
+    </ul>
+</body>
+</html>
+
+<?php
+    function ShowPostData($post, $rarity){
+        $text = htmlspecialchars($post['text'], ENT_QUOTES, 'UTF-8');
+        $username = htmlspecialchars($post['username'], ENT_QUOTES, 'UTF-8');
+        $date = htmlspecialchars($post['date']);
+        if($rarity === 1){
+            return '<li><h1><font color="red">'.$text.' ('.$username.') - '.$date.'</font></h1>';
+        }else if($rarity === 2){
+            return '<li><h2>'.$text.' ('.$username.') - '.$date.'</h2>';
+        }else if($rarity === 3){
+            return '<li><h3>'.$text.' ('.$username.') - '.$date.'</h3>';
+        }else if($rarity === 4){
+            return '<li>'.$text.' ('.$username.') - '.$date;
+        }else{
+            return "Error: Do'nt exist number of $rarity";
+        }
+    }
+?>
---
+++ input.php
@@ -0,0 +1,86 @@
+<?php
+//SQLに使用する用語の定義
+define("DB_HOST", "localhost");
+define("DB_NAME", "bbs_php");
+define("DB_CHARSET", "utf8");
+define("DB_USER", "root");
+define("DB_PASSWORD", "BBS_php17");
+define("DB_TABLE", "posts");
+
+//rarityに使用する数値
+define("SS_RARE", 4);
+define("S_RARE", 16);
+define("RARE", 41);
+
+//使用するデフォルトのタイムゾーンを設定
+date_default_timezone_set('UTC');
+$date_time = new DateTime();
+
+//タイムゾーンを東京に設定
+$date_time->setTimeZone(new DateTimeZone('Asia/Tokyo'));
+if(!isset($date_time)){
+    echo "datetime is Error!!!";
+    exit;
+}
+
+try{
+    //DBへの接続
+    $db = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset='.DB_CHARSET, DB_USER, DB_PASSWORD,
+    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));        //DB接続できた物に対する、操作を行う.今回はエラーレポートに対し、例外を投げるように設定している(これにより try, catchが使用可能)
+
+    //postデータを受け取った際の処理
+    $id = $_POST['id'];
+    $user_name = trim($_POST['user']);
+    $message = trim($_POST['message']);
+
+    $db_post = $db->query("select * from posts");
+
+    if(isset($_POST) && !empty($user_name) && !empty($message)){
+         //DB接続
+        $sql_text = $db->prepare('insert into '.DB_TABLE.' (date, username, text, rarity) values (?, ?, ?, ?)');
+
+        //レア度の決定
+        $rare_num = rand(1, 100);
+        $rare = random_rarity($rare_num);
+
+        //用意したmysql用の変数に具体的な値を代入する
+        $sql_text->bindValue(1, $date_time->format('Y-m-d H:i:s'));
+        $sql_text->bindValue(2, $user_name);
+        $sql_text->bindValue(3, $message);
+        $sql_text->bindValue(4, $rare, PDO::PARAM_INT);
+        //実行をセット
+        $sql_text->execute();
+
+        // $sql_text->execute(
+        //     array(
+        //         ':date'=>$date_time->format('Y-m-d H:i:s'),
+        //         ':username'=>$user_name,
+        //         ':text'=>$message,
+        //         ':rarity'=>$rare,
+        //     )
+        // );
+    }
+    $db = null;
+}
+catch(PODException $e){
+    echo "Error: read mysql";
+    exit('データベース接続失敗'.$e->getMessage());
+}
+
+header('Location: '.$_POST["url"]);
+
+
+//レア度を決めるメソッド
+function random_rarity($rare_num){
+    if($rare_num < SS_RARE){
+        return 1;
+    }else if($rare_num < S_RARE){
+        return 2;
+    }else if($rare_num < RARE){
+        return 3;
+    }else{
+        return 4;
+    }
+}
+
+?>
